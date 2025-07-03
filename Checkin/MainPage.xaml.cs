5642e5e1149dc0cecfd7993523abf0c87622cf98
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2025-07-03 11:24:49 -0400

    adding buttons

--- Checkin/MainPage.xaml.cs
+++ Checkin/MainPage.xaml.cs
@@ -3,10 +3,27 @@
 public partial class MainPage : ContentPage
 {
     int count = 0;
+    private IDispatcherTimer _timer;
+    private Boolean CheckedIn;
+    private DateTime ElapsedTime;
 
     public MainPage()
     {
         InitializeComponent();
+        _timer = Application.Current.Dispatcher.CreateTimer();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        ClockDisplay.Text = DateTime.Now.ToString("HH:mm:ss");
+    }
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _timer.Stop();
     }
 
     private void OnCounterClicked(object? sender, EventArgs e)
@@ -19,5 +36,11 @@ public partial class MainPage : ContentPage
             CounterBtn.Text = $"Clicked {count} times";
 
         SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void Checkin(object? sender, EventArgs e)
+    {
+        CheckedIn = true;
+
     }
 }
---
+++ Checkin/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Checkin;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
