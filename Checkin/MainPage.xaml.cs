fdba643f079453a34c899e9d912929d776cd8590
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2025-07-05 20:36:50 -0400

    reworking a lot

--- Checkin/MainPage.xaml.cs
+++ Checkin/MainPage.xaml.cs
@@ -1,11 +1,15 @@
-﻿using Checkin.Models;
+﻿using Bumptech.Glide.Manager;
+using Checkin.Models;
 using Checkin.ViewModel;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Checkin;
 
 public partial class MainPage : ContentPage
 {
     private IDispatcherTimer _timer;
+    public ContextModel CurrentContext {  get; set; }
     //public List<ContextModel> ContextModels = [];
 
     public MainPage(MainViewModel viewModel, IDispatcherTimer timer)
@@ -22,6 +26,7 @@ public partial class MainPage : ContentPage
     private void Timer_Tick(object sender, EventArgs e)
     {
         ClockDisplay.Text = DateTime.Now.ToString("HH:mm:ss");
+
     }
     protected override void OnDisappearing()
     {
@@ -40,5 +45,142 @@ public partial class MainPage : ContentPage
     //    }; 
     //    ContextModels.Add(defaultContext);
     //}
+
+    //IDispatcherTimer timer;
+    //public CheckinViewModel(IDispatcherTimer timer)
+    //{
+    //    Properties = new ContextPropertiesModel();
+
+
+    //}
+    public bool CheckedOut { get; set; }
+    public bool CheckedIn { get; set; }
+    public ContextPropertiesModel Properties { get; set; }
+
+    public required List<ContextModel> ContextModels { get; set; }
+
+    private async Task<List<ContextModel>> GetContextData()
+    {
+        var dataCount = 1;
+        //var storedData = await SecureStorage.Default.GetAsync("1");
+        while (dataCount > 0)
+        {
+            var storedData = await SecureStorage.Default.GetAsync(dataCount.ToString());
+            if (storedData != null)
+            {
+                var formattedData = FormatStorageData(storedData);
+                ContextModels.Add(formattedData);
+                dataCount++;
+            }
+            else
+            {
+                dataCount = 0;
+                ContextModels.Add(InitializeContext());
+                continue;
+            }
+
+        }
+        var cm = new List<ContextModel>();
+        if (storedData != null)
+        { // if we find data in the local storage
+
+            return cm;
+        }
+        else
+        {
+            var defaultContext = new ContextModel()
+            {
+                Id = 1,
+                Name = "Default",
+                Checks = [],
+            };
+            cm.Add(defaultContext);
+            return cm;
+        }
+    }
+
+    public ContextModel InitializeContext()
+    {
+        return new ContextModel()
+        {
+             Id = 1,
+             Name = "Default",
+             Checks = [],
+             Icon = null,
+        };
+    }
+
+    public ContextModel FormatStorageData(string storedData)
+    {
+        if (!string.IsNullOrEmpty(storedData))
+        {
+            ContextModel cm = JsonSerializer.Deserialize<ContextModel>(storedData);
+            if (cm == null)
+            {
+                // possible error. TODO log this if it happens
+                return InitializeContext();
+            }
+            return cm;
+        }
+        else
+        {
+            return InitializeContext();
+        }
+    }
+    public void Checkin(object? sender, EventArgs e)
+    {
+        CheckedIn = true;
+        var timeStamp = new CheckModel()
+        {
+            CheckedIn = CheckedIn,
+            CheckedTime = DateTime.Now,
+        };
+        ContextModels[0].Checks?.Add(timeStamp);
+        // save timestamp to ContextCheckLog
+        // when holding down, provide option to select custom Checkin time
+    }
+
+    public void Checkout(object? sender, EventArgs e)
+    {
+        CheckedIn = false;
+        var timeStamp = new CheckModel()
+        {
+            CheckedIn = CheckedIn,
+            CheckedTime = DateTime.Now,
+        };
+        ContextModels[0].Checks?.Add(timeStamp);
+        // save timestamp to ContextCheckLog
+        // when holding down, provide option to select custom Checkout time
+    }
+
+    public void GetSummary(object? sender, EventArgs e)
+    {
+        var dateTime1 = ContextModels[0]?.Checks[1]?.CheckedTime;
+        var dateTime2 = ContextModels[0]?.Checks[0]?.CheckedTime;
+        var result = dateTime1 - dateTime2;
+        TestLabel.Text = result.ToString();
+        // if still checked in, show the current live time
+        // Nice to Have: also update the live preferences (money, rate, etc)
+        // otherwise, show calculated time and the calculated preference
+    }
+
+    public bool IsCheckedIn(object? sender, EventArgs e)
+    {
+        var len = ContextModels[0]?.Checks.Count ?? 0;
+        if (ContextModels[0]?.Checks[len]?.CheckedIn == true)
+        {
+            CheckedIn = true;
+            CheckedOut = false;
+            return CheckedIn;
+        }
+        else
+        {
+            CheckedIn = false;
+            CheckedOut = true;
+            return CheckedIn;
+        }
+    }
+
+
 
 }
